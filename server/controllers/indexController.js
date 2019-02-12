5480ccf97c772abefb64a79eb137c35838f73783
khaledmoez <khaledmoez@yahoo.fr>
2019-02-12 11:14:16 +0100

    login et signup

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,13 +1,96 @@
-// body(firstName, lastName, email, password, role) - Création d’un user
+let User = require('../models/user')
+let passport = require('passport')
+let userRoleEnum = require('../config/userRoles')
+
+const { body, validationResult } = require('express-validator/check')
+const { sanitizeBody } = require('express-validator/filter')
+
+// 01   body(firstName, lastName, email, password, role) - Création d’un user
 exports.index_signup_post = [
+  body('role').optional().isInt().withMessage('user role must be a number')
+    .isIn(userRoleEnum.arrayUserRoles).withMessage(userRoleEnum.errorMessage),
+
+  // Sanitize fields.
+  sanitizeBody('*').trim().escape(),
+
   (req, res, next) => {
-    res.send('NOT IMPLEMENTED: index_signup_post')
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      return res.status(422).json({ code: '422', message: errors.array()[0]['param'] + ' : ' + errors.array()[0]['msg'] })
+    } else {
+      // Check if USer with same email already exists.
+      User.findOne({ 'user_email': req.body.email }).exec(function (err, foundUser) {
+        if (err) { return next(err) }
+
+        if (foundUser) {
+          // user exists
+          return res.status(422).json({ code: '422', message: 'user with email ' + foundUser.user_email + ' already exists' })
+        } else {
+          User.create({
+            user_first_name: req.body.firstName,
+            user_family_name: req.body.familyName,
+            user_email: req.body.email,
+            user_password: req.body.password,
+            user_role: req.body.role
+          },
+          function (err, user) {
+            if (err) {
+              return res.status(500).send({ code: '500', message: 'There was a problem adding the user to the database: ' + err.message })
+            }
+            // res.status(200).send(user);
+            res.status(200).send({ code: '200', message: 'Création d\'utilisateur réussie.' })
+          })
+        }
+      })
+    }
   }
 ]
 
-// body(email, password) - Authentification d’un user
+// 02   body(email, password) - Authentification d’un user
 exports.index_login_post = [
+  body('email').isLength({ min: 1 }).trim().withMessage('User email must be specified.')
+    .isEmail().withMessage('User email must be a valid mail.'),
+  body('password', 'user password must be specified').isLength({ min: 1 }).trim().withMessage('User password must be specified.'),
+
+  sanitizeBody('*').trim().escape(),
+
   (req, res, next) => {
-    res.send('NOT IMPLEMENTED: index_login_post')
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      return res.status(422).json({ code: '422', message: errors.array()[0]['param'] + ' : ' + errors.array()[0]['msg'] })
+    } else {
+      User.findOne({ user_email: req.body.email }, function (err, user) {
+        if (err) return res.status(500).send({ code: '500', message: 'Error on the server.' })
+        if (!user) return res.status(404).send({ code: '404', message: 'No user found.' })
+
+        return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+          if (err) {
+            return next(err)
+          }
+
+          if (passportUser) {
+            const user = passportUser
+            user.token = passportUser.generateJWT()
+
+            // return res.json({ user: user.toAuthJSON() })
+            // return res.json(passportUser.generateJWT())
+
+            return res.status(200).json({
+              message: 'connexion réussie',
+              token: passportUser.generateJWT()
+            })
+          }
+
+          // return res.status(400).info;
+          return res.status(400).send({ code: '400', message: info.errors })
+        })(req, res, next)
+      })
+    }
   }
 ]
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,96 @@
 let mongoose = require('mongoose')
-let Schema = mongoose.Schema
 
-let UserSchema = new Schema()
+let Schema = mongoose.Schema
+let bcrypt = require('bcryptjs')
+
+let jwt = require('jsonwebtoken')
+let userRoleEnum = require('../config/userRoles').userRoleEnum
+
+let subSchemaFollowedCourse = new mongoose.Schema(
+  {
+    course_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
+      required: [true, 'user\'s course is mandatory']
+    }
+  },
+  {
+    _id: false
+  }
+)
+
+let UserSchema = new Schema(
+  {
+    user_first_name: {
+      type: String,
+      required: [true, 'user first name is mandatory']
+    },
+    user_family_name: {
+      type: String,
+      required: [true, 'user last name is mandatory']
+    },
+    user_email: {
+      type: String,
+      required: [true, 'user email is mandatory'],
+      match: [/\S+@\S+\.\S+/, 'User email must be a valid mail format'],
+      unique: true,
+      index: true
+    },
+    user_password: {
+      type: String,
+      required: [true, 'user password is mandatory']
+    },
+    user_role: {
+      type: Number,
+      required: [true, 'user role is mandatory'],
+      default: userRoleEnum.get('student').value,
+      validate: [
+        (v) => userRoleEnum.isDefined(v),
+        require('../config/userRoles').errorMessage
+      ]
+    },
+    user_followed_courses: [subSchemaFollowedCourse]
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  }
+)
+
+// Virtual for user's full name
+UserSchema
+  .virtual('user_virtual_full_name')
+  .get(function () {
+    return this.user_family_name + ', ' + this.user_first_name
+  })
+
+UserSchema.pre('save', function (next) {
+  // this represente le user qui s'apprete a ete inséré
+  // check if password is present and is modified.
+  if (this.user_password && this.isModified('user_password')) {
+    // call your hashPassword method here which will return the hashed password.
+    this.user_password = bcrypt.hashSync(this.user_password, 8)
+  }
+
+  next() // everything is done, so let's call the next callback.
+})
+
+UserSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compareSync(candidatePassword, this.user_password)
+}
+
+UserSchema.methods.generateJWT = function () {
+  const today = new Date()
+  const expirationDate = new Date(today)
+  expirationDate.setDate(today.getDate() + 60)
+
+  return jwt.sign({
+    email: this.user_email,
+    id: this._id,
+    role: this.user_role,
+    exp: parseInt(expirationDate.getTime() / 1000, 10)
+  }, process.env.SECRET)
+}
 
 // Export model
 module.exports = mongoose.model('User', UserSchema)
