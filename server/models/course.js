f24e6b10bc484b881c68964212a144c00e170124
melkihaythem <melkihaythem@users.noreply.github.com>
2019-02-12 10:49:02 +0100

    fix

--- server/models/course.js
+++ server/models/course.js
@@ -1,7 +1,17 @@
 let mongoose = require('mongoose')
-let Schema = mongoose.Schema
+let Schema = mongoose.Schema ;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
-let CourseSchema = new Schema()
+
+let CourseSchema = new Schema({
+    course_titre: {type:string} ,
+    course_description: {type:string},
+    course_enonce: {type:string},
+    course_statut : {type:number},
+    course_teacher : {type:ObjectId,ref:'User'},
+    course_date : {type:Date,default:Date.now}
+
+})
 
 // Export model
 module.exports = mongoose.model('Course', CourseSchema)
