bfa4f347736db8bce3dbdf131636e9610b33641a
MariemNasraoui <MariemNasraoui@users.noreply.github.com>
2019-02-14 12:10:26 +0100

    modif

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,7 +40,7 @@ exports.user_course_get = [
 // 04 - Mettre à jour un course  (id_course présent dans body) ecrit par ce user. l'id du user sera récupéré du token
 exports.user_course_update_post = [
   async (req, res, next) => {
-    let id = { _id: ObjectId(req.params.id_courses) }
+    let id = { _id: ObjectId(req.params.id_course) }
     const result = await Course.findByIdAndUpdate(id, { $set: req.body }).exec().catch(err => err)
     res.send({ msg: "changed", result })
     // res.send('NOT IMPLEMENTED: user_course_update_post')
@@ -50,7 +50,7 @@ exports.user_course_update_post = [
 // 05 - Suppression d'un course ecrit par ce user  (id_course présent dans body). l'id du user sera récupéré du token
 exports.user_course_delete_post = [
   (req, res, next) => {
-    let id = { _id: ObjectId(req.params.id_courses) }
+    let id = { _id: ObjectId(req.params.id_course) }
     Course.findByIdAndRemove(id).catch(err => err)
     res.send("deleted")
     // res.send('NOT IMPLEMENTED: user_course_delete_post')
@@ -60,49 +60,78 @@ exports.user_course_delete_post = [
 // 06 - follow a course (id_course présent dans body). Le votant sera ce même user. l'id du user sera récupéré du token
 exports.user_course_follow_post = [
   (req, res, next) => {
+    
     res.send('NOT IMPLEMENTED: user_course_follow_post')
   }
 ]
 
 // 07 - Récupérer les détails d’un user. l'id du user sera récupéré du token. On recupere aussi la liste des commentaires ecrits par ce user
 exports.user_get = [
-  (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user_get')
+  async (req, res, next) => {
+    let id = { _id: ObjectId(req.params.id_user) }
+    const result = await User.findOne(id).catch(err => err)
+    res.send(result)
+    // res.send('NOT IMPLEMENTED: user_get')
   }
 ]
 
 // 08 - Mettre à jour de ce même user. l'id du user sera récupéré du token
 exports.user_update_post = [
-  (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user_update_post')
+  async (req, res, next) => {
+    let id = { _id: ObjectId(req.params.id_user) }
+  req.body.password = bcrypt.hashSync(this.password);
+  const result = await User.findOneAndUpdate(id, req.body).catch(err => err)
+  res.send(result);
+    // res.send('NOT IMPLEMENTED: user_update_post')
   }
 ]
 
 // 09 - Suppression de ce même user. l'id du user sera récupéré du token
 exports.user_delete_post = [
-  (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user_delete_post')
+  async (req, res, next) => {
+    let id = { _id: ObjectId(req.params.id_user) }
+  const result = await User.findByIdAndRemove(id).catch(err => err)
+  res.send(result);
+    // res.send('NOT IMPLEMENTED: user_delete_post')
   }
 ]
 
 // 10 - Créer un commentaire sur un course (id_article présent dans body). Le commentateur sera ce même user. l'id du user sera récupéré du token
 exports.user_comment_create_post = [
-  (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user_comment_create_post')
+  async (req, res, next) => {
+    let id = { _id: ObjectId(req.params.id_Course) }
+  console.log(req.params.id);
+  const result = await comment.create(req.body).catch(err => err)
+  const Course = await Course.updateOne(id, { $push: { comment: result } }).catch(err => err)
+  res.send(Course);
+    // res.send('NOT IMPLEMENTED: user_comment_create_post')
   }
 ]
 
 // 11 - Mettre à jour un comment ecrit par ce user (id_user récupéré depuis le token). id_comment present dans body.
 exports.user_comment_update_post = [
-  (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user_comment_update_post')
+  async (req, res, next) => {
+    let id = { _id: ObjectId(req.params.id_user) }
+  const result = await comment.findOneAndReplace(id, req.body).catch(err => err)
+  res.send(result);
+
+    // res.send('NOT IMPLEMENTED: user_comment_update_post')
   }
 ]
 
 // 12 - Suppression d'un comment ecrit par ce user (id_user récupéré depuis le token). id_comment present dans body
 exports.user_comment_delete_post = [
-  (req, res, next) => {
-    res.send('NOT IMPLEMENTED: user_comment_delete_post')
+  async (req, res, next) => {
+    let ID = ObjectId(req.params.id_Course);
+    let comment_index = req.params.index_comment;
+    let result = await Course.updateOne({ _id: ID }, { $unset: { [`comment.${comment_index}`]: 1 } });
+    console.log(result);
+    result = await Course.updateOne({ _id: ID }, { $pull: { comment: null } });
+    await Course.updateOne(id, { $pull: { comment: result } }).catch(err => err)
+    console.log(result);
+    res.send(result)
+  
+    // res.send('NOT IMPLEMENTED: user_comment_delete_post')
   }
 ]
 
