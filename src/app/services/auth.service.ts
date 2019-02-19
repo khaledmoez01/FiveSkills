0885e42a80264f17c190ba1dc32eb77d2a510cb5
MariemNasraoui <MariemNasraoui@users.noreply.github.com>
2019-02-19 10:04:07 +0100

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
 
   login(user): Observable<any>  {
   
-      return this.http.post('http://localhost:3000/signup', user);
+      return this.http.post('http://localhost:3001/login', user);
    
   }
 }
